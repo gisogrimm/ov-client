72b936f222ff0350aaa289a5ad227ea2c3004fc8
Giso Grimm <giso.grimm@vegri.net>
2020-10-04 08:45:13 +0200

    update archilinux build test

--- docker/archlinux/entrypoint.sh
+++ docker/archlinux/entrypoint.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 cd /build
-# update / clone TASCAR:
-test -e tascar || git clone https://github.com/gisogrimm/tascar
-(
-    cd tascar
-    git pull
-    # build TASCAR:
-    make -j 4
-    # install TASCAR:
-    make install
-)
 # update / clone ov-client:
-test -e ov-clientr || git clone https://github.com/gisogrimm/ov-client
+test -e ov-client || git clone https://github.com/gisogrimm/ov-client
 (
     cd ov-client
     git pull
+    git checkout $1
     make
 )
