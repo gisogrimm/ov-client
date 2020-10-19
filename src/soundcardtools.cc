29346a5e60186189e3da8c6decf6dcf8240ce04c
Alessandro Aresta <performerstone@gmail.com>
2020-10-19 13:27:24 +0200

    added miniaudio; soundcardtools device listing using miniaudio

--- src/soundcardtools.cc
+++ src/soundcardtools.cc
@@ -3,6 +3,9 @@
 #include <alsa/asoundlib.h>
 #endif
 
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+
 std::vector<snddevname_t> list_sound_devices()
 {
   std::vector<snddevname_t> retv;
@@ -39,6 +42,48 @@ std::vector<snddevname_t> list_sound_devices()
   // Free hint buffer too
   snd_device_name_free_hint((void**)hints);
 #endif
+
+//#ifdef __APPLE__
+    ma_result result;
+    ma_context context;
+    ma_device_info* pPlaybackDeviceInfos;
+    ma_uint32 playbackDeviceCount;
+    ma_device_info* pCaptureDeviceInfos;
+    ma_uint32 captureDeviceCount;
+    ma_uint32 iDevice;
+
+    if (ma_context_init(NULL, 0, NULL, &context) != MA_SUCCESS) {
+        printf("Failed to initialize context.\n");
+        //return -2;
+    }
+
+    result = ma_context_get_devices(&context, &pPlaybackDeviceInfos, &playbackDeviceCount, &pCaptureDeviceInfos, &captureDeviceCount);
+    if (result != MA_SUCCESS) {
+        printf("Failed to retrieve device information.\n");
+        //return -3;
+    }
+
+    printf("Playback Devices\n");
+    for (iDevice = 0; iDevice < playbackDeviceCount; ++iDevice) {
+        printf("    %u: %s\n", iDevice, pPlaybackDeviceInfos[iDevice].name);
+        snddevname_t dname;
+      dname.dev = pPlaybackDeviceInfos[iDevice].name;
+      //dname.desc = desc;
+
+      retv.push_back(dname);
+    }
+
+    printf("\n");
+
+    printf("Capture Devices\n");
+    for (iDevice = 0; iDevice < captureDeviceCount; ++iDevice) {
+        printf("    %u: %s\n", iDevice, pCaptureDeviceInfos[iDevice].name);
+    }
+
+
+    ma_context_uninit(&context);
+//#endif // __APPLE__
+
   return retv;
 }
 
