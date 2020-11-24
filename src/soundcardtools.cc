d456f8bbb66e414c5695302633257b8d710a6a0b
Giso Grimm <giso.grimm@vegri.net>
2020-11-24 21:15:12 +0100

    in case of empty list also report non hw: soundcards

--- src/soundcardtools.cc
+++ src/soundcardtools.cc
@@ -41,6 +41,33 @@ std::vector<snddevname_t> list_sound_devices()
   }
   // Free hint buffer too
   snd_device_name_free_hint((void**)hints);
+  if(retv.empty()) {
+    /* Enumerate sound devices */
+    err = snd_device_name_hint(-1, "pcm", (void***)&hints);
+    if(err != 0) {
+      std::cerr << "Warning: unable to get name hints (list_sound_devices)"
+                << std::endl;
+      return retv;
+    }
+    n = hints;
+    while(*n != NULL) {
+      name = snd_device_name_get_hint(*n, "NAME");
+      desc = snd_device_name_get_hint(*n, "DESC");
+      snddevname_t dname;
+      dname.dev = name;
+      dname.desc = desc;
+      if(dname.desc.find("\n"))
+        dname.desc.erase(dname.desc.find("\n"));
+      retv.push_back(dname);
+      if(name && strcmp("null", name))
+        free(name);
+      if(desc && strcmp("null", desc))
+        free(desc);
+      n++;
+    }
+    // Free hint buffer too
+    snd_device_name_free_hint((void**)hints);
+  }
 #endif
   return retv;
 }
