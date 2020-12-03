090b3ccc590928e0f27f1c79f10d0048a97f9f74
Giso Grimm <giso.grimm@vegri.net>
2020-12-03 08:37:19 +0100

    test for higher order parent process for ovbox detection

--- src/ov_tools.cc
+++ src/ov_tools.cc
@@ -11,6 +11,29 @@
 #endif
 
 #ifdef LINUX
+
+static pid_t get_process_parent_id(const pid_t pid)
+{
+  pid_t ppid(0);
+  char buffer[BUFSIZ];
+  sprintf(buffer, "/proc/%d/stat", pid);
+  FILE* fp = fopen(buffer, "r");
+  if(fp) {
+    size_t size = fread(buffer, sizeof(char), sizeof(buffer), fp);
+    if(size > 0) {
+      // See: http://man7.org/linux/man-pages/man5/proc.5.html section
+      // /proc/[pid]/stat
+      strtok(buffer, " ");              // (1) pid  %d
+      strtok(NULL, " ");                // (2) comm  %s
+      strtok(NULL, " ");                // (3) state  %c
+      char* s_ppid = strtok(NULL, " "); // (4) ppid  %d
+      ppid = atoi(s_ppid);
+    }
+    fclose(fp);
+  }
+  return ppid;
+}
+
 static void get_process_name(const pid_t pid, char* name, char* arg)
 {
   name[0] = '\0';
@@ -37,7 +60,7 @@ bool is_ovbox()
 {
 #ifdef LINUX
   // check if parent is the autorun script:
-  pid_t ppid(getppid());
+  pid_t ppid(get_process_parent_id(get_process_parent_id(getppid())));
   char name[BUFSIZ];
   char arg[BUFSIZ];
   get_process_name(ppid, name, arg);
