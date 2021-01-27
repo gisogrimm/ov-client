3e88c8e79367b86b7fcca43f939a26716537c4ca
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-01-27 01:05:28 +0100

    feature: build zita on osx

--- build_osx.sh
+++ build_osx.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+cmake zita/zita-resampler-1.6.2
+cd zita/zita-resampler-1.6.2 && cmake . && make
+
+
+cd zita/zita-njbridge-0.4.4 && cmake . && make
+
 OPENSSL_ROOT=$(brew --prefix openssl) CPPREST_ROOT=$(brew --prefix cpprestsdk) BOOST_ROOT=$(brew --prefix boost) make
