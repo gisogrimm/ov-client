29530d8420ff1fb5fbb562a0b8937628f718d114
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-01-29 03:22:28 +0100

    Updated build and run script

--- build_and_run_osx.sh
+++ build_and_run_osx.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 OPENSSL_ROOT=$(brew --prefix openssl) CPPREST_ROOT=$(brew --prefix cpprestsdk) BOOST_ROOT=$(brew --prefix boost) NLOHMANN_JSON_ROOT=$(brew --prefix nlohmann-json) make
 (cd zita/zita-resampler-1.6.2 && cmake . && make)
-(cd zita/zita-njbridge-0.4.4 && cmake . && make && cp zita-n2j ../../ && cp zita-j2n ../../)
+(cd zita/zita-njbridge-0.4.4 && cmake . && make && cp zita-n2j ../../build/ && cp zita-j2n ../../build/)
 
 ./start_ov-client.sh -f ds
