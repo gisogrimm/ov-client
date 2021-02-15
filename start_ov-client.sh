c87d5460a30226bd882e85858faf6e227b371b07
delude88 <tobias.hegemann@googlemail.com>
2021-02-15 16:51:34 +0100

    fix: path corrected inside start_ov-client.sh

--- start_ov-client.sh
+++ start_ov-client.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PATH=./build:./build/zita:$PATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/plugins:./tascar/plugins/build/ ./build/ov-client $*
+PATH=./build:$PATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/plugins:./tascar/plugins/build/ ./build/ov-client $*
