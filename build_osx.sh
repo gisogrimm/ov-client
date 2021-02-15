fe27251ce2e235bf0b5bd33b1a6d395e8333e127
delude88 <tobias.hegemann@googlemail.com>
2021-02-15 14:28:37 +0100

    fix: github submodule update inside osx build script

--- build_osx.sh
+++ build_osx.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+git submodule update --init --recursive
 mkdir -p build
 cd build
 OPENSSL_ROOT_DIR=$(brew --prefix openssl) CPPREST_ROOT=$(brew --prefix cpprestsdk) BOOST_ROOT=$(brew --prefix boost) NLOHMANN_JSON_ROOT=$(brew --prefix nlohmann-json) cmake ./..
