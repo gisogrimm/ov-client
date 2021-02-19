64cb6da5e947a59b4eda13c87c7d3ed5dfb6ec6e
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-02-19 21:37:01 +0100

    Current work state: need rethinking about monorepo

--- build_osx.sh
+++ build_osx.sh
@@ -6,6 +6,8 @@ mkdir -p build
 cd build
 OPENSSL_ROOT_DIR=$(brew --prefix openssl) CPPREST_ROOT=$(brew --prefix cpprestsdk) BOOST_ROOT=$(brew --prefix boost) NLOHMANN_JSON_ROOT=$(brew --prefix nlohmann-json) cmake ./..
 make
+mv zita/zita-nj2 .
+mv zita/zita-j2n .
 # OPENSSL_ROOT=$(brew --prefix openssl) CPPREST_ROOT=$(brew --prefix cpprestsdk) BOOST_ROOT=$(brew --prefix boost) NLOHMANN_JSON_ROOT=$(brew --prefix nlohmann-json) make
 # (cd zita/zita-resampler-1.6.2 && cmake . && make)
 # (cd zita/zita-njbridge-0.4.4 && cmake . && make && cp zita-n2j ../../build/ && cp zita-j2n ../../build/)
