d2fdaab8440455ee9f97c90c14eb7322790c4af4
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-02-20 04:48:42 +0100

    Updated macOS specific install

--- build_osx.sh
+++ build_osx.sh
@@ -6,5 +6,4 @@ mkdir -p build
 cd build
 OPENSSL_ROOT_DIR=$(brew --prefix openssl) CPPREST_ROOT=$(brew --prefix cpprestsdk) BOOST_ROOT=$(brew --prefix boost) NLOHMANN_JSON_ROOT=$(brew --prefix nlohmann-json) cmake ./..
 make
-mv zita/zita-n2j .
-mv zita/zita-j2n .
+make install
