97c86a2b38ef170a1d5e3e3fa781a442eaa03fa6
Giso Grimm <giso.grimm@vegri.net>
2021-05-02 10:17:51 +0200

    add copyright notice

--- src/ov-client_hostname.cc
+++ src/ov-client_hostname.cc
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020 Giso Grimm
+ */
+/*
+ * ov-client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, version 3 of the License.
+ *
+ * ov-client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHATABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License, version 3 for more details.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * Version 3 along with ov-client. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <udpsocket.h>
 
 int main(int argc, char** argv)
