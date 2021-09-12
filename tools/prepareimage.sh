0e167956c44a647ae2484ec2b76af1bd095d813c
Giso Grimm <giso.grimm@vegri.net>
2021-09-12 11:30:08 +0200

    allow creation of DS-frontend-clients

--- tools/prepareimage.sh
+++ tools/prepareimage.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 imagefile mount-directory" >&2
     exit 1
@@ -16,6 +16,7 @@ if ! [ -d "$2" ]; then
     exit 1
 fi
 #
+SUFFIX=ovinstaller
 IMGBASE=$(basename "$1")
 sudo losetup -f -P "$1"
 DEVNAME=$(losetup -l|grep -F -e "${IMGBASE}"|tail -1|sed -e 's/[[:blank:]].*//1')
@@ -37,6 +38,17 @@ SRCPATH=`dirname $0`
     sudo sed -i -e "\+init=/usr/lib/raspi-config/init_resize+ s/.*/sed -i \'s| init=\/usr\/lib\/raspi-config\/init_resize\\\.sh| init=\/usr\/lib\/raspi-config\/install_ovclient\\\.sh|\' \/boot\/cmdline.txt/1" "$2/usr/lib/raspi-config/init_resize.sh"
 )
 sudo umount "$2"
+if [ "${DIGITALSTAGE}" = "yes" ]; then
+    # add config file to /boot partition
+    SUFFIX=ds-ovinstaller
+    sudo mount ${DEVNAME}p1 "$2"
+    echo '{"protocol":"ov","ui":"https://digital-stage.ovbox.de/","url":"http://digital-stage-device.ovbox.de/"}' | sudo tee "$2/ov-client.cfg"
+    sudo umount "$2"
+fi
 sync
 sudo losetup -d ${DEVNAME}
-#zip ${IMGBASE/.img}.zip ${1}
+SRC="${1}"
+echo "renaming to ${SRC/.img}-${SUFFIX}.img"
+mv "${SRC}" "${SRC/.img}-${SUFFIX}.img"
+echo "compressing to ${IMGBASE/.img}-${SUFFIX}.zip"
+zip "${IMGBASE/.img}-${SUFFIX}.zip" "${SRC/.img}-${SUFFIX}.img"
