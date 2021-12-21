6da14a4f614a0ff3056c0356c7e992551235644e
Giso Grimm <giso.grimm@vegri.net>
2021-12-21 11:34:48 +0100

    add missing tascar plugins, correct path

--- start_ov-client.sh
+++ start_ov-client.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PATH=./build:$PATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/plugins/:libov/tascar/plugins/build/:libov/tascar/libtascar/build/ $DBGTOOL ./build/ov-client $*
+PATH=./build:$PATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/plugins/:./build/libov/tascar/plugins/:libov/tascar/plugins/build/:libov/tascar/libtascar/build/ $DBGTOOL ./build/ov-client $*
