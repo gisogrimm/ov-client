a36bd5f80acb237e7e7bdf08ca1336d1eecc309c
Giso Grimm <giso@Gisos-Mac-mini.local>
2022-01-22 09:58:32 +0100

    list sound cards on mac

---
+++ start_env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/plugins/:./build/libov/tascar/plugins/:libov/tascar/plugins/build/:libov/tascar/libtascar/build/
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$LD_LIBRARY_PATH
+PATH=./build:$PATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./build/plugins/:./build/libov/tascar/plugins/:libov/tascar/plugins/build/:libov/tascar/libtascar/build/ $DBGTOOL $*
