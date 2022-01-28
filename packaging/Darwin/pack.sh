48195831c12b64196b3b1c41c17d78b703eeb6f7
Giso Grimm <giso.grimm@vegri.net>
2022-01-28 09:52:45 +0100

    correct cp flags in installer
    cp on Linux and Darwin accepts a slightly different set of options

--- packaging/Darwin/pack.sh
+++ packaging/Darwin/pack.sh
@@ -11,6 +11,6 @@ fi
 rm -Rf "${PKG}" "${PKG}_`uname -s`_`uname -m`.tgz"
 mkdir "${PKG}"
 cat "${PKG}.csv" | sed -e "s/[^,]*,[[:blank:]]*/${PKG}\//1" -e 's/\/\.\//\//1' -e 's/\/$//1' |sort -u|xargs -L 1 -- mkdir -p
-cat "${PKG}.csv" | sed -e "s/\([^,]*\),[[:blank:]]*/..\/..\/\1 ${PKG}\//1" -e 's/\/\.\//\//1' -e 's/\/$//1'|xargs -I % -- sh -c "cp -ar %"
+cat "${PKG}.csv" | sed -e "s/\([^,]*\),[[:blank:]]*/..\/..\/\1 ${PKG}\//1" -e 's/\/\.\//\//1' -e 's/\/$//1'|xargs -I % -- sh -c "cp -a %"
 tar czf "${PKG}_`uname -s`_`uname -m`.tgz" "${PKG}"
 rm -Rf "${PKG}"
