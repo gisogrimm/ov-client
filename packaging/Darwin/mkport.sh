848630e4fbab9529110c5360a4a267813821c721
Giso Grimm <giso@Gisos-Mac-mini.local>
2022-01-28 16:55:21 +0100

    pack libraries recursively

--- packaging/Darwin/mkport.sh
+++ packaging/Darwin/mkport.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 BIN="$1"
 BINDIR=$(dirname "$BIN")
+SCRIPT=`pwd`/$0
+SCRIPT=$(echo $SCRIPT | sed -e 's/.*\/\//\//1' -e 's/\/\.\//\//1')
+echo $SCRIPT
 (
     cd ${BINDIR}
     echo "cd  ${BINDIR}; current dir:"
     pwd
     BINBASE=$(echo "$BIN" | awk -F'/' '{print $NF}')
-    mkdir -p lib
+#    mkdir -p lib
     for lib in `otool -L $BINBASE | sed -e "/dylib/! d" -e "/@loader_path/ d" -e "s/[[:blank:]]*\//\//1" -e "s/dylib .*/dylib/1" -e "/\//! d"`; do
-	echo "copying $lib to lib"
-	cp $lib lib/
 	OBJ_LEAF_NAME=$(echo "$lib" | awk -F'/' '{print $NF}')
-	if test -e "lib/${OBJ_LEAF_NAME}"; then
-	    echo "replacing  $lib by lib/${OBJ_LEAF_NAME} in ${BINBASE}"
-	    install_name_tool -change "$lib" "lib/${OBJ_LEAF_NAME}" "${BINBASE}"
+	if test "$OBJ_LEAF_NAME" != "$BINBASE"; then
+	    echo "copying $lib to ./"
+	    cp $lib ./
+	    if test -e "${OBJ_LEAF_NAME}"; then
+		echo "replacing  $lib by ${OBJ_LEAF_NAME} in ${BINBASE}"
+		install_name_tool -change "$lib" "${OBJ_LEAF_NAME}" "${BINBASE}"
+		$SCRIPT "${OBJ_LEAF_NAME}"
+	    fi
 	fi
     done
-    chmod u+w lib/*
+    chmod u+w *
 )
