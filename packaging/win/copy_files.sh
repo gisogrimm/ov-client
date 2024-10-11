aa0fb24d5fb710cb57804dbc200df6661d358607
Giso Grimm (win32) <g.grimm@uni-oldenburg.de>
2024-10-11 10:46:14 +0200

    correct file mode to binary for download, correct packaging on win

--- packaging/win/copy_files.sh
+++ packaging/win/copy_files.sh
@@ -18,5 +18,11 @@ cat "$1"|sed -e "/#.*/ d" -e  "s/.*,/${BASENAME}\//1"|sort -u|xargs -l mkdir -p
 cat "$1"|(cd ../.. && sed -e "/^[ \t]*$/ d" -e "/#.*/ d" -e "s|\(.*\),\(.*\)|cp -r \1 ${STARTDIR}/${BASENAME}/\2|1")
 # copy files:
 cat "$1"|(cd ../.. && sed -e "/^[ \t]*$/ d" -e "/#.*/ d" -e "s|\(.*\),\(.*\)|cp -r \1 ${STARTDIR}/${BASENAME}/\2|1"|bash)
+
+# make portable package:
+MSYSFILES=$(cygcheck ${BASENAME}/bin/* ${BASENAME}/lib/gdk-pixbuf-2.0/*/*.dll 2>/dev/null|grep msys64|sed 's/^ *//1' | sort -bu)
+cp ${MSYSFILES} ${BASENAME}/bin/
+glib-compile-schemas ${BASENAME}/bin/share/glib-2.0/schemas
+
 # package files:
 zip -r "${BASENAME}-$2.zip" "${BASENAME}"
