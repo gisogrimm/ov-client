43e3f2adcaf80587139e39605f659d9b06f7e322
Giso Grimm <giso.grimm@vegri.net>
2025-02-28 23:02:38 +0100

    do not build glabsensors on windows, always compile LSL stuff

--- docker/ubuntu2204/entrypoint.sh
+++ docker/ubuntu2204/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-cd /build
-# update / clone ov-client:
-test -e ov-client || git clone https://github.com/gisogrimm/ov-client
-(
-    cd ov-client
-    git pull
-    git checkout $1
-    git submodule update --init --recursive
-    make
-    make packaging
-    find -name "*.deb" -ls
-)
+cd /build/ov-client
+# build and package command line tools:
+make -j $(nproc) && make -C packaging/deb pack
