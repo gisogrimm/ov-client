de47f67e23ad9181e04b4d4e6edb925299f8a47b
Giso Grimm <giso.grimm@vegri.net>
2025-05-18 16:42:17 +0200

    update docker file to test compilation on Fedora

--- docker/fedora/entrypoint.sh
+++ docker/fedora/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-cd /build
-# update / clone ov-client:
-test -e ov-client || git clone https://github.com/gisogrimm/ov-client
-(
-    cd ov-client
-    git pull
-    git checkout $1
-    git submodule update --init --recursive
-    make
-)
+#cd /build
+## update / clone ov-client:
+#test -e ov-client || git clone https://github.com/gisogrimm/ov-client
+#(
+#    cd ov-client
+#    git pull
+#    git checkout $1
+#    git submodule update --init --recursive
+#    make
+#)
+#
+cd build/ov-client
+make
