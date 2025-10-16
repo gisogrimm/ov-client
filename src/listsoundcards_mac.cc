edcdcce94d9543bdd43084dcbe0c5ddd673d5cc3
Giso Grimm <giso.grimm@vegri.net>
2025-10-16 19:57:05 +0200

    do not show levelanalysis port as input port candidate

--- src/listsoundcards_mac.cc
+++ src/listsoundcards_mac.cc
@@ -1,75 +1,57 @@
 #include <CoreAudio/CoreAudio.h>
 #include <iostream>
 #include <string>
-
-void PrintAudioDeviceProperty(AudioDeviceID deviceID, const AudioObjectPropertyAddress& propertyAddress) {
-    UInt32 dataSize = 0;
-    OSStatus status = AudioObjectGetPropertyDataSize(deviceID, &propertyAddress, 0, NULL, &dataSize);
-    if (status != noErr) {
-        std::cerr << "Error getting property data size: " << status << std::endl;
-        return;
-    }
-
-    char buffer[1024];
-    if (dataSize > sizeof(buffer)) {
-        std::cerr << "Buffer too small for property data" << std::endl;
-        return;
-    }
-
-    status = AudioObjectGetPropertyData(deviceID, &propertyAddress, 0, NULL, &dataSize, buffer);
-    if (status != noErr) {
-        std::cerr << "Error getting property data: " << status << std::endl;
-        return;
-    }
-
-    std::cout << buffer << std::endl;
-}
-
-void PrintAudioDeviceName(AudioDeviceID deviceID) {
-    AudioObjectPropertyAddress propertyAddress = {
-        kAudioDevicePropertyDeviceNameCFString,
-        kAudioDevicePropertyScopeOutput,
-        kAudioObjectPropertyElementMaster
-    };
-
-    PrintAudioDeviceProperty(deviceID, propertyAddress);
-}
-
-void EnumerateAudioDevices() {
-    AudioObjectPropertyAddress propertyAddress = {
-        kAudioHardwarePropertyDevices,
-        kAudioObjectPropertyScopeGlobal,
-        kAudioObjectPropertyElementMaster
-    };
-
-    UInt32 dataSize = 0;
-    OSStatus status = AudioObjectGetPropertyDataSize(kAudioObjectSystemObject, &propertyAddress, 0, NULL, &dataSize);
-    if (status != noErr) {
-        std::cerr << "Error getting property data size: " << status << std::endl;
-        return;
-    }
-
-    UInt32 deviceCount = dataSize / sizeof(AudioDeviceID);
-    AudioDeviceID* devices = new AudioDeviceID[deviceCount];
-
-    status = AudioObjectGetPropertyData(kAudioObjectSystemObject, &propertyAddress, 0, NULL, &dataSize, devices);
-    if (status != noErr) {
-        std::cerr << "Error getting property data: " << status << std::endl;
-        delete[] devices;
-        return;
-    }
-
-    std::cout << "Audio Devices:" << std::endl;
-    for (UInt32 i = 0; i < deviceCount; ++i) {
-        std::cout << "Device " << i + 1 << ": ";
-        PrintAudioDeviceName(devices[i]);
-    }
-
-    delete[] devices;
-}
+#include <vector>
 
 int main() {
-    EnumerateAudioDevices();
+    // Get the number of audio devices
+    UInt32 deviceCount = 0;
+    OSStatus status = AudioHardwareGetProperty(kAudioHardwarePropertyDeviceCount, &deviceCount);
+    if (status != noErr) {
+        std::cerr << "Error getting device count." << std::endl;
+        return 1;
+    }
+
+    if (deviceCount == 0) {
+        std::cerr << "No audio devices found." << std::endl;
+        return 1;
+    }
+
+    // List all audio devices
+    for (UInt32 i = 0; i < deviceCount; ++i) {
+        AudioDeviceID deviceId = 0;
+        UInt32 dataSize = sizeof(AudioDeviceID);
+        status = AudioHardwareGetProperty(kAudioHardwarePropertyDeviceIDs,
+                                         sizeof(UInt32) + (i * sizeof(UInt32)),
+                                         &dataSize,
+                                         &deviceId);
+        if (status != noErr) {
+            std::cerr << "Error getting device ID for index " << i << std::endl;
+            continue;
+        }
+
+        // Get the device name
+        CFStringRef deviceName = nullptr;
+        dataSize = sizeof(CFStringRef);
+        status = AudioDeviceGetProperty(deviceId,
+                                      0,
+                                      false,
+                                      kAudioDevicePropertyDeviceName,
+                                      &dataSize,
+                                      &deviceName);
+        if (status != noErr) {
+            std::cerr << "Error getting device name for device ID " << deviceId << std::endl;
+            continue;
+        }
+
+        // Convert CFStringRef to std::string
+        if (deviceName) {
+            std::string name = CFStringGetCString(deviceName, kCFStringEncodingUTF8);
+            std::cout << "Device ID: " << deviceId << ", Name: " << name << std::endl;
+            CFRelease(deviceName);
+        }
+    }
+
     return 0;
 }
 
